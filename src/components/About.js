a98b8e750056fbfc3da9dd282dbe81182e1af7f3
Kennith Chan <kennithdchan@gmail.com>
2018-03-27 18:41:32 -0400

    Added more person info

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../Site.css';
-
 class About extends Component {
   /*
   constructor(props) {
@@ -13,11 +12,13 @@ class About extends Component {
       <div className="About-root">
         <h1 className="About-title">About This Site</h1>
         <p className="About-text">
-          This site is a quick demonstration of ReactJS. It also serves as a quick reference for coffee lore.
+          This site was built using <a href="https://reactjs.org/">ReactJS</a> and <a href="https://p5js.org/">p5.js</a>.
+          Photos are public domain taken from <a href="https://www.pexels.com/public-domain-images/">pexels.com</a>.
         </p>
         <h1 className="About-title">About Me</h1>
         <p className="About-text">
-          I&#39;m just a simple programmer who makes projects. Coffee is always involved one way or another.
+          My name is Ken, and I am a programmer with a Bachelors of Engineering in Computer Engineering. I have three years of experience working with a variety of computer systems.
+          These range from drone-mounted computer vision systems to blackbox frontend testing automation. My main interests are automation and system design, but this list is far from exhaustive.
         </p>
         <h2 className="About-title">Contact Info</h2>
         <p className="About-text">
